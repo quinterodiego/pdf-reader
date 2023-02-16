e32c11169263301ddcd05126a4b966411d45e506
quinterodiego <d86webs@gmail.com>
2023-02-16 12:10:37 -0300

    agrego proceso para fci

--- server.js
+++ server.js
@@ -1,43 +1,44 @@
 const express = require('express');
-const multer = require('multer');
+// const multer = require('multer');
 const fs = require("fs");
 const { PdfReader } = require("pdfreader");
-const path = require('path');
+// const path = require('path');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-let storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, 'c:/temp');
-    },
-    filename: function(req, file, cb) {
-        cb(null, file.originalname);
-    }
-});
+// let storage = multer.diskStorage({
+//     destination: function(req, file, cb) {
+//         cb(null, 'c:/temp');
+//     },
+//     filename: function(req, file, cb) {
+//         cb(null, file.originalname);
+//     }
+// });
 
-let upload = multer({ storage: storage })
+// let upload = multer({ storage: storage })
 
-console.log(upload.storage.getFilename)
+// console.log(upload.storage.getFilename)
 
-app.post('/api/rename', upload.array('myFiles', 12), (req, res, next) => {
-    const files = req.files;
-    if(!files) {
-        const error = new Error('Elija al menos un archivo');
-        error.httpStatusCode = 400;
-        return next(error);
-    }
-    res.send({
-        archivos: files
-    })
-});
+// app.post('/api/rename', upload.array('myFiles', 12), (req, res, next) => {
+//     const files = req.files;
+//     if(!files) {
+//         const error = new Error('Elija al menos un archivo');
+//         error.httpStatusCode = 400;
+//         return next(error);
+//     }
+//     res.send({
+//         archivos: files
+//     })
+// });
 
 app.get('/api/rename/pdfs/:anio/:mes/:dia', async (req, res) => {
     const { anio, mes, dia } = req.params
     const fileNames = await fs.readdirSync(`\\\\daredevil-cl\\Documentos\\Fin\\${anio}\\${mes}\\${dia}`);
-    const nuevoArray = await fileNames.filter((file) => file.match('.pdf'))
+    const fileNamesToLowerCase = fileNames.map(file => file.toLocaleLowerCase())
+    const nuevoArray = await fileNamesToLowerCase.filter((file) => file.match('.pdf'))
     console.log(nuevoArray)
     function readFile(file){
         const arr = []
@@ -58,7 +59,6 @@ app.get('/api/rename/pdfs/:anio/:mes/:dia', async (req, res) => {
         n = n + ''; 
         return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n; 
     }
-    
 
     nuevoArray.forEach((i, index) => {    
         readFile(`\\\\daredevil-cl\\Documentos\\Fin\\${anio}\\${mes}\\${dia}\\${i}`)
@@ -68,8 +68,13 @@ app.get('/api/rename/pdfs/:anio/:mes/:dia', async (req, res) => {
                     (err) {
                         if ( err ) console.log('ERROR: ' + err)
                     });
+                } else if (result[21].includes('LIQUIDACION') && result[21].includes('CUOTAPARTES')) {
+                    fs.rename(`\\\\daredevil-cl\\Documentos\\Fin\\${anio}\\${mes}\\${dia}\\${i}`, `\\\\daredevil-cl\\Documentos\\Fin\\${anio}\\${mes}\\${dia}\\fci${pad(result[22].slice(16, 24), 8)}.pdf`, function
+                    (err) {
+                        if ( err ) console.log('ERROR: ' + err)
+                    });
                 } else {
-                    console.log('No es un boleto')
+                    console.log('No boleto o fci')
                 }
             })
     });
